a7431220c5ce58ca9f6c78fb9bee500babe9bcab
Justin Basinger <justin.m.basinger@gmail.com>
2017-02-12 10:09:19 -0500

    Adding login button

--- www/js/controllers/googleAuthCtrl.js
+++ www/js/controllers/googleAuthCtrl.js
@@ -1,4 +1,13 @@
 angular.module('app.controllers')
-.controller('GoogleAuthCtrl', function($scope) {
+.controller('GoogleAuthCtrl', function($scope, GoogleApi, $state) {
 
-})
+  $scope.login = function(){
+    GoogleApi.isAuthorized().then(function(result){
+      var month = parseInt(moment().format("M"))-1;
+      $state.go('app.monthlyReport', {month: month});
+    }, function(result){
+      console.log(result);
+    });
+  };
+
+});
